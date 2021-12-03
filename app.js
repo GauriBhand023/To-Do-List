89e51d229e560ce15155db48120227a66d2cc8b1
Gauri Bhand <htcgauribhand@gmail.com>
2021-12-03 23:55:43 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+let items=["Buy Food", "Cook Food", "Eat Food"];
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended :true}));
+app.use(express.static("public"))
+
+app.get("/", function(req, res){
+    let today = new Date();
+    
+    let options={
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+    };
+
+    let day = today.toLocaleDateString("en-us", options);
+
+    res.render("list", {kindOfDay: day, newListItems: items});
+});
+
+app.post("/", function(req,res){
+    let item=req.body.newItem;
+    items.push(item);
+    res.redirect("/")
+})
+
+app.listen(3000, function(){
+    console.log("Server is running on port 3000");
+});
